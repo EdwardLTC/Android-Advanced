6cb73ed6573a7759b166336bb10025d1f15e1671
EdwardLTC <88788257+EdwardLTC@users.noreply.github.com>
2022-11-01 10:49:44 +0700

    update

--- MyApplication/app/src/main/java/com/edward/myapplication/adapter/DialogCallback.java
+++
@@ -1,7 +0,0 @@
-package com.edward.myapplication.adapter;
-
-import android.widget.EditText;
-
-public interface DialogCallback {
-    void onClickYes(EditText name, EditText price, EditText amount, EditText des);
-}
--- MyApplication/app/src/main/java/com/edward/myapplication/adapter/RecycleCallBack.java
+++
@@ -1,7 +0,0 @@
-package com.edward.myapplication.adapter;
-
-import com.edward.myapplication.models.User;
-
-public interface RecycleCallBack {
-    void onClickComputer(User user);
-}
